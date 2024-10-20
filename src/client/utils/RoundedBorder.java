28f17eeb21ee567e388b94b0b970c88599dc9422
binhdq <dangbinh1770@gmail.com>
2024-10-20 17:03:10 +0700

    add giao dien MainMenu

---
+++ src/client/utils/RoundedBorder.java
@@ -0,0 +1,28 @@
+package client.utils;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class RoundedBorder implements Border {
+    private int radius;
+    private Color color;  // Add color field
+
+    public RoundedBorder(int radius, Color color) {
+        this.radius = radius;
+        this.color = color;  // Initialize the color
+    }
+
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius + 1, this.radius + 1, this.radius + 1, this.radius + 1);
+    }
+
+    public boolean isBorderOpaque() {
+        return true;
+    }
+
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        g.setColor(color);  // Set the border color
+        g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+    }
+}
